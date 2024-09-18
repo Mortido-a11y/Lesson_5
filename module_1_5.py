934dd123c94371073c0db68b283acb8ef1845d12
Denis Zenkov <id90814@gmail.com>
2024-09-19 02:21:50 +0300

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,5 +1,6 @@
 immutable_var = (1, 2, [3, 4], 'string', False)
 print(immutable_var)
+# кортежи являются неизменяеммым типом данных, поэтому при попытке изменить элемент кортежа, например, immutable_var[0] = 5, программа выдает ошибку, которая сообщает, что кортеж не поддерживает обращение по элементам
 mutable_list = [1, 2,  'string', False]
 mutable_list[0] = True
 print(mutable_list)
